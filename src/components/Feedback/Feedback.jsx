4977e2381b30d9405ef19074d935c03124c1b00d
Oleksandr <alex.vovchok@gmail.com>
2023-04-10 00:26:08 +0300

    start task1

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import css from './Feedback.module.css';
+
+class Feedback extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+      }
+
+      handleIncrementGood = () => {
+        this.setState(prevState => ({ good: prevState.good + 1,
+        }));
+      }
+      handleIncrementNeutral = () => {
+        this.setState(prevState => {
+            return {
+                neutral: prevState.neutral + 1,
+            };
+        });
+      }
+      handleIncrementBad = () => {
+        this.setState(prevState => {
+            return {
+                bad: prevState.bad + 1,
+            };
+           
+        });
+        
+      }
+
+      countTotalFeedback = () => {}
+      
+      render() {
+const goodFeedback = this.state.good;
+const neutralFeedback = this.state.neutral;
+const badFeedback = this.state.bad;
+
+
+        return (
+            <div>
+                <h1>Please leave feedback</h1>
+                <div className={css.boxButton}>
+                    <button type='button' onClick={this.handleIncrementGood}>Good</button>
+                    <button type='button' onClick={this.handleIncrementNeutral}>Neutral</button>
+                    <button type='button' onClick={this.handleIncrementBad}>Bad</button>
+                </div>
+                <h2>Statistics</h2>
+                <ul>
+                    <li>Good:{this.state.good}</li>
+                    <li>Neutral:{this.state.neutral}</li>
+                    <li>Bad:{this.state.bad}</li>
+                    <li>{goodFeedback + neutralFeedback}</li>
+                    </ul>
+            </div>
+        )
+      }
+}
+
+export default Feedback;
